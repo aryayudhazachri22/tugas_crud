4470221b857b84b27c85a6234677a53cd04601da
aryayudhazachri22 <aryayudhazachri09@gmail.com>
2021-08-10 20:25:08 +0700

    tugas crud

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,47 @@
+@extends('users.master')
+@section('content')
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <a href="{{ route('blog.create') }}" class="d-none d-sm-inline-block btn btn-dark shadow-sm"><i
+        class="text-white"></i> Tambah Data Baru [+]</a>
+    </div>
+
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+        @endif
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Gambar</th>
+                <th scope="col">Judul</th>
+                <th scope="col">Content</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+    @foreach($blogs as $blog)
+        <tbody>
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>
+                    <img src="{{ url('/img/'.$blog->gambar) }}" class="rounded" style="width: 150px">
+                </td>
+                <td>{{$blog['judul']}}</td>
+                <td>{{$blog['content']}}</td>
+                <td>
+                    <form action="{{ route('blog.destroy',$blog->id) }}" method="POST">
+                        <a class="btn btn-success" href="{{ route('blog.edit',$blog->id) }}">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Hapus</button>
+                    </form>
+                </td>
+            </tr>
+        </tbody>
+    @endforeach
+    </table>
+@endsection
+ 
